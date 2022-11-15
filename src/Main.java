5d162e64502fa13df24d9e4b4fd2ac51c5c8ba93
rocha <rjoao0112@gmail.com>
2022-11-15 10:18:43 +0000

    EX1

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //exercise1();
+
+
+
+    }
+
+    public static void exercise1(){
+        String[] informations = new String[]{"32423424", "Joao Rocha"};
+
+        System.out.println("Name: "+informations[1]+"\n" +
+                "ID: "+informations[0]);
+    }
+
+    public static void exercise2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many day's do you want rent the car!");
+        switch (sc.ne()){
+            case
+        }
+    }
+
+    public static void exercise3(){
+
+    }
+
+    public static void exercise4(){
+
+    }
+}

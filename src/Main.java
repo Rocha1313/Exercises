214ac63a4c6c5bbb864aae2c1e6420c6faef7a21
rocha <rjoao0112@gmail.com>
2022-11-15 10:39:58 +0000

    Ex3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        //exercise1();
-
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
 
 
     }
@@ -19,13 +22,39 @@ public class Main {
     public static void exercise2(){
         Scanner sc = new Scanner(System.in);
         System.out.println("How many day's do you want rent the car!");
-        switch (sc.ne()){
-            case
+        int days = sc.nextInt();
+        int payOut = 35;
+
+        if(days == 1){
+            System.out.println("The price for "+days+" day is "+payOut+"$");
+            return;
         }
+
+        if(days >= 7){
+            payOut = (days * payOut)-40;
+            System.out.println("The price for "+days+" day's is "+payOut+"$");
+            return;
+        }
+
+        payOut = (days * payOut)-10;
+        System.out.println("The price for "+days+" day's is "+payOut+"$");
+
     }
 
     public static void exercise3(){
+        int total = 1;
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(7);
+        numbers.add(8);
 
+        for (int n : numbers){
+            total = total * n;
+        }
+
+        System.out.println("The Total of: "+ Arrays.toString(numbers.toArray()) +"\n" +
+                "Is: "+total);
     }
 
     public static void exercise4(){
